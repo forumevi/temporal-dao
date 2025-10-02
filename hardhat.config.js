3b519c6a03faf58d942432302755b39060773855
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-03 00:30:36 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,8 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     somnia: {
-      url: "https://rpc.somnia.network",
+      url: "https://api.infra.mainnet.somnia.network", // ✅ Çalışan RPC
+      chainId: 5031, // ✅ Doğru ChainID
       accounts: [process.env.PRIVATE_KEY],
     }
   }
